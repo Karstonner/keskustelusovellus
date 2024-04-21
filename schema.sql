8575a8caa7d3a9a69f3bffec798084a97e44ccc6
tomppaa <jonne.perseinen@gmail.com>
2024-04-21 16:40:30 +0300

    Tarkennuksia

--- schema.sql
+++ schema.sql
@@ -1,7 +1,12 @@
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS threads CASCADE;
+DROP TABLE IF EXISTS topics CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
-	tunnus TEXT UNIQUE,
-	salasana TEXT,
+	tunnus TEXT UNIQUE CHECK(tunnus IS NOT NULL AND length(tunnus) > 1 AND length(tunnus) < 15),
+	salasana TEXT CHECK (salasana IS NOT NULL AND length(salasana) > 8),
 	is_admin INTEGER
 );
 CREATE TABLE topics (
@@ -12,12 +17,12 @@ CREATE TABLE threads (
 	id SERIAL PRIMARY KEY,
 	topic INTEGER REFERENCES topics,
 	creator INTEGER REFERENCES users,
-	title TEXT,
-	opening TEXT
+	title TEXT CHECK(title IS NOT NULL AND length(title) > 1 AND length(title) < 50),
+	opening TEXT CHECK(opening IS NOT NULL AND length(opening) > 1 AND length(opening) < 280)
 );
 CREATE TABLE messages (
 	id SERIAL PRIMARY KEY,
-	content TEXT,
+	content TEXT CHECK(content IS NOT NULL AND length(content) > 1 AND length(content) < 280),
 	topic TEXT,
 	thread INTEGER REFERENCES threads,
 	user_id INTEGER REFERENCES users,
