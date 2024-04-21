e39c9fa00abe794e489aa949283457d35ea7a4d2
Karstonner <157248726+Karstonner@users.noreply.github.com>
2024-04-21 16:16:09 +0300

    Lisätty tauluja

--- schema.sql
+++ schema.sql
@@ -1,6 +1,25 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	tunnus TEXT UNIQUE,
+	salasana TEXT,
+	is_admin INTEGER
+);
+CREATE TABLE topics (
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE
+);
+CREATE TABLE threads (
+	id SERIAL PRIMARY KEY,
+	topic INTEGER REFERENCES topics,
+	creator INTEGER REFERENCES users,
+	title TEXT,
+	opening TEXT
+);
 CREATE TABLE messages (
 	id SERIAL PRIMARY KEY,
 	content TEXT,
 	topic TEXT,
+	thread INTEGER REFERENCES threads,
+	user_id INTEGER REFERENCES users,
 	sent_at TIMESTAMP
 );
