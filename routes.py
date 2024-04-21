552c8ccb3fe9917705741a553a6dfce1901abe9c
tomppaa <jonne.perseinen@gmail.com>
2024-04-21 15:58:22 +0300

    Lisätty kirjautuminen ja rekisteröityminen

--- routes.py
+++ routes.py
@@ -1,13 +1,45 @@
 from app import app
 from flask import render_template, request, redirect
 from sqlalchemy.sql import text
-import messages
+import messages, users
 topics = ["musiikki", "ruoka", "yliopisto"]
 
 @app.route("/")
 def index():
     list = messages.get_list()
     return render_template("index.html", count=len(list), messages=list)
+
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        tunnus = request.form["tunnus"]
+        salasana = request.form["salasana"]
+        if users.login(tunnus, salasana):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Jokin meni pieleen")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        tunnus = request.form["tunnus"]
+        salasana1 = request.form["salasana1"]
+        salasana2 = request.form["salasana2"]
+        if salasana1 != salasana2:
+            return render_template("error.html", message="Jokin meni pieleen")
+        if users.register(tunnus, salasana1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Jokin meni pieleen")
     
 @app.route("/musiikki")
 def musiikki():
